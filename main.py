1d8b22aaec1b0f697de6940352ede66c9b9fc8e5
Fiattarone <Fiattarone@me.com>
2022-04-09 21:31:19 -0700

    Testing pixela endpoint

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
 
-response = requests.post("PixelHabitTracker")
-response = requests.get("PixelHabitTracker")
+pixela_endpoint ="https://pixe.la/v1/users"
+
+user_params = {
+    "token": "a random token",
+    "username": "random_user",
+    "agreeTermsOfService": "maybe",
+    "notMinor": "maybe"
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
