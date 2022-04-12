698f8b00eda8b0b6fc85987679de05d8023ae105
Fiattarone <Fiattarone@me.com>
2022-04-11 21:06:46 -0700

    Testing graph endpoint

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import requests
 
 pixela_endpoint ="https://pixe.la/v1/users"
+TOKEN = "a_random_token"
+USER_NAME = "d4v1d"
 
 user_params = {
-    "token": "a random token",
-    "username": "random_user",
-    "agreeTermsOfService": "maybe",
-    "notMinor": "maybe"
+    "token": TOKEN,
+    "username": USER_NAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+
+graph_endpoint = f"{pixela_endpoint}/{USER_NAME}/graph"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Programming Graph",
+    "unit": "days",
+    "type": "integer",
+    "color": "kuro"
+}
+
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+
 print(response.text)
